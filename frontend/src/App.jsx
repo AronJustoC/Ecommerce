eaf20000adb23885542083033530f572d8057d57
AronJustoC <aronchoque727@gmail.com>
2025-02-15 13:23:34 -0500

    fix: resolved Tailwind and DaisyUI issues
    Implemented global state management for products.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { use, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import NavBar from './components/NavBar.jsx';
-import HomePage from './pages/HomePage.jsx';
-import ProductPage from './pages/ProductPage.jsx';
+import NavBar from "./components/NavBar.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 function App() {
+  const { theme } = useThemeStore();
   return (
-    <div className='min-h-screen bg-base-200 transition-colors duration-300'>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme={theme}>
       <NavBar />
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/product/:id' element={<ProductPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </div>
   )
